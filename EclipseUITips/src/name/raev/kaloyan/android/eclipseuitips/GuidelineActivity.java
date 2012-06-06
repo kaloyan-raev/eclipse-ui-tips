da215002c87f4463d62df111b75f36ef937ff101
Kaloyan Raev <kaloyan@raev.name>
2012-06-06 21:55:28 +0300

    [2] Add copyright headers in the source files

--- EclipseUITips/src/name/raev/kaloyan/android/eclipseuitips/GuidelineActivity.java
+++ EclipseUITips/src/name/raev/kaloyan/android/eclipseuitips/GuidelineActivity.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Kaloyan Raev.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Kaloyan Raev - initial implementation
+ *******************************************************************************/
 package name.raev.kaloyan.android.eclipseuitips;
 
 import name.raev.kaloyan.android.eclipseuitips.R;
